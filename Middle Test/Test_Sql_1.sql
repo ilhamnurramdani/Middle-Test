32cd45d6f53adef95abf72b93ed309082bb58bac
ilhamnurramdani <119870825+ilhamnurramdani@users.noreply.github.com>
2024-04-15 20:49:15 +0700

    Add files via upload

---
+++ Middle Test/Test_Sql_1.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT salary from employee_table
+ORDER by salary DESC
+LIMIT 1 offset 2
---
+++ Middle Test/Test_Sql_2.sql
@@ -0,0 +1,7 @@
+﻿SELECT item from (
+		SELECT item, COUNT(*) as cnt
+  	from item_bought 
+  	GROUP by item
+) 
+WHERE cnt != (SELECT max(cnt) FROM (SELECT item, COUNT(*) as cnt from item_bought GROUP by item))
+and cnt != (SELECT min(cnt) from (SELECT item, COUNT(*)as cnt FROM item_bought GROUP BY item))
